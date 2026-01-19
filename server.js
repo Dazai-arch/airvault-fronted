91a82e4ee12fe0d92b171d3141e304b926a80b15
Alok Tayal <113999153+Dazai-arch@users.noreply.github.com>
2026-01-20 02:07:58 +0530

    Change CORS origin to allow all origins

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("trust proxy", 1);
 // ====================================
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: true,
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
