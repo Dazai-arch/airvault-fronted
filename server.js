bba0228a36ad6ce61e191abc1c63e67b8d540d24
Alok Tayal <alok.tayal@mca.christuniversity.in>
2026-01-21 00:37:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -177,9 +177,9 @@ const AuditLog = mongoose.model('AuditLog', auditLogSchema);
 // EMAIL CONFIGURATION
 // ====================================
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  service: process.env.EMAIL_SERVICE || 'smtp.gmail.com',
+  port: Number(process.env.EMAIL_PORT),
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
