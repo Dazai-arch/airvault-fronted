ae2c3b10c58d18cf5a5da2ae1329284ca636e23d
Alok Tayal <113999153+Dazai-arch@users.noreply.github.com>
2026-01-20 23:51:41 +0530

    Change email service to use host instead of service

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ const AuditLog = mongoose.model('AuditLog', auditLogSchema);
 // EMAIL CONFIGURATION
 // ====================================
 const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE || 'smtp.gmail.com',
+  host: process.env.EMAIL_SERVICE || 'smtp.gmail.com',
   port: Number(process.env.EMAIL_PORT),
   secure: false,
   auth: {
