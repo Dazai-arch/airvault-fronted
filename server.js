13796bbdc632b369c215d136142ffcb8232c9b4c
Alok Tayal <alok.tayal@mca.christuniversity.in>
2026-01-21 19:20:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.set("trust proxy", 1);
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Allow requests with no origin (mobile apps, Postman, etc.)
       if (!origin) return callback(null, true);
 
       const allowedOrigins = [
@@ -37,8 +36,11 @@ app.use(
         "http://localhost:5173",
         "http://localhost:3000",
       ];
+      
+      const isAllowed = allowedOrigins.indexOf(origin) !== -1 || 
+                        (origin && origin.endsWith('.vercel.app'));
 
-      if (allowedOrigins.indexOf(origin) !== -1) {
+      if (isAllowed) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
